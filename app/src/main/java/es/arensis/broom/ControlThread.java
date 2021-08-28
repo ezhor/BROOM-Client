6ac33105163ee73c619208a50036b69e7574a541
Ézhor Malkávar <ezhormalkavar@gmail.com>
2021-08-28 17:10:29 +0200

    Control communication from Android to BROOM looks solid now.

--- app/src/main/java/es/arensis/broom/ControlThread.java
+++ app/src/main/java/es/arensis/broom/ControlThread.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class ControlThread extends Thread {
 
@@ -30,8 +31,14 @@ public class ControlThread extends Thread {
             OutputStream outputStream = socket.getOutputStream();
 
             while (active) {
-                outputStream.write(BroomStatus.getInstance().toBytes());
-                Log.e("BroomStatus", BroomStatus.getInstance().toString());
+                outputStream.write(BroomStatus.getInstance().toString().getBytes());
+                /*Log.e("BroomStatus",
+                        BroomStatus.getInstance().toString().substring(0, 4)
+                        + "|" + BroomStatus.getInstance().toString().substring(4, 8)
+                        + "|" + BroomStatus.getInstance().toString().substring(8, 12)
+                        + "|" + BroomStatus.getInstance().toString().substring(12, 16)
+                        + "|" + BroomStatus.getInstance().toString().substring(16, 17)
+                );*/
                 Thread.sleep(DELAY);
             }
             socket.close();
